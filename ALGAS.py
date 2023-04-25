b00046396c0c5cf00dfc2dda0011614a46113980
Felipe Cruz <79320575+FelipeCruz777@users.noreply.github.com>
2023-04-25 17:37:38 -0300

    Update ALGAS.py

--- ALGAS.py
+++ ALGAS.py
@@ -1,9 +1,7 @@
 import time
 import random
 import pyodbc
-import geocoder
 from sys import getsizeof
-
 
 conn_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:serer-cruz.database.windows.net,1433;Database=algas-cruz;Uid=adm;Pwd=Urubu100@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 conn = pyodbc.connect(conn_string)
@@ -11,7 +9,7 @@ cursor = conn.cursor()
 
 cursor.execute("IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'temperatura' AND COLUMN_NAME = 'memoria') ALTER TABLE temperatura ADD memoria INT")
 
-sizes = range(200000, 200001, 10000)
+sizes = range(200000, 200300, 10000)
 l1 = []
 
 for n in sizes:
@@ -31,7 +29,8 @@ for n in sizes:
     l1.append(stop - start)
 
 l2 = []
-for n in sizes:
+for i in range(30):
+    n = random.choice(sizes)
     data = b'x' * n
     b = memoryview(data)
     start = time.time()
@@ -44,13 +43,13 @@ for n in sizes:
             min_mem = getsizeof(b) - getsizeof(b'')
         b = b[1:]
     temperature = random.randint(20, 40)
-    g = geocoder.ip('me')
-    location = g.latlng
-    sql = "INSERT INTO temperatura (temperatura, latitude, longitude, memoria) VALUES (?, ?, ?, ?)"
-    val = (temperature, location[0], location[1], max_mem)
+    location = "norte da virginia"
+    sql = "INSERT INTO temperatura (temperatura, regiao, memoria) VALUES (?, ?, ?)"
+    val = (temperature, location, max_mem)
     cursor.execute(sql, val)
     cursor.commit()
     stop = time.time()
     print(f'Temperatura {n} {stop-start} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
     l2.append(stop - start)
+    
 cursor.close()
