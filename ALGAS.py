a605aeba52e7b1821fee7fb12c98570a9068c219
Felipe Cruz <79320575+FelipeCruz777@users.noreply.github.com>
2023-04-25 17:47:30 -0300

    Update ALGAS.py

--- ALGAS.py
+++ ALGAS.py
@@ -46,10 +46,11 @@ for i in range(30):
     location = "norte da virginia"
     sql = "INSERT INTO temperatura (temperatura, regiao, memoria) VALUES (?, ?, ?)"
     val = (temperature, location, max_mem)
+    start_insert = time.time()
     cursor.execute(sql, val)
     cursor.commit()
-    stop = time.time()
-    print(f'Temperatura {n} {stop-start} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
-    l2.append(stop - start)
+    stop_insert = time.time()
+    print(f'Temperatura {n} {stop_insert-start_insert} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
+    l2.append(stop_insert - start_insert)
     
 cursor.close()
