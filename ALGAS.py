bebc32bb7223894907ee791ae549755814ef7f1c
Felipe Cruz <79320575+FelipeCruz777@users.noreply.github.com>
2023-04-26 17:34:04 -0300

    Update ALGAS.py

--- ALGAS.py
+++ ALGAS.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-from sys import getsizeof
 import time
 import random
 import pyodbc
+from sys import getsizeof
+import matplotlib.pyplot as plt
 
 conn_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:serer-cruz.database.windows.net,1433;Database=algas-cruz;Uid=adm;Pwd=Urubu100@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 conn = pyodbc.connect(conn_string)
@@ -10,8 +10,9 @@ cursor = conn.cursor()
 
 cursor.execute("IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'temperatura' AND COLUMN_NAME = 'memoria') ALTER TABLE temperatura ADD memoria INT")
 
-sizes = range(200000, 200001, 10000)
+sizes = range(200000, 200300, 10000)
 l1 = []
+l2 = []
 
 for n in sizes:
     data = 'transaction01' * n
@@ -29,8 +30,8 @@ for n in sizes:
     print(f'Valor {n} {stop-start} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
     l1.append(stop - start)
 
-l2 = []
-for n in sizes:
+for i in range(30):
+    n = random.choice(sizes)
     data = b'x' * n
     b = memoryview(data)
     start = time.time()
@@ -43,18 +44,22 @@ for n in sizes:
             min_mem = getsizeof(b) - getsizeof(b'')
         b = b[1:]
     temperature = random.randint(20, 40)
-    sql = "INSERT INTO temperatura (temperatura, 'Virginia', memoria) VALUES (?, ?, ?)"
-    val = (temperature, 'Localização da máquina', max_mem)
+    location = "Maua"
+    sql = "INSERT INTO temperatura (temperatura, location, memoria) VALUES (?, ?, ?)"
+    val = (temperature, location, max_mem)
+    start_insert = time.time()
     cursor.execute(sql, val)
     cursor.commit()
-    stop = time.time()
-    print(f'Temperatura {n} {stop-start} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
-    l2.append(stop - start)
+    stop_insert = time.time()
+    print(f'Temperatura {n} {stop_insert-start_insert} - Max mem {max_mem/10**3} KB - Min mem {min_mem} B')
+    l2.append(stop_insert - start_insert)
+    
 cursor.close()
 
-plt.plot(l1,'x--', label="Without Memoryview")
-plt.plot(l2,'o--', label="With Memoryview")
-plt.xlabel('Size of Bytearray')
-plt.ylabel('Time (S)')
+plt.plot(sizes, l1, label='Loop 1')
+plt.plot(sizes, l2, label='Loop 2')
+plt.xlabel('Tamanho da entrada (bytes)')
+plt.ylabel('Tempo de execução (s)')
+plt.title('Gráfico de tempo de execução')
 plt.legend()
 plt.show()
