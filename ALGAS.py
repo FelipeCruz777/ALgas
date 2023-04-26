bf4fbf23d3ea04ea7abac87c1e6f17d3da9e35f3
Felipe Cruz <79320575+FelipeCruz777@users.noreply.github.com>
2023-04-26 18:22:05 -0300

    Update ALGAS.py

--- ALGAS.py
+++ ALGAS.py
@@ -45,7 +45,7 @@ for i in range(30):
         b = b[1:]
     temperature = random.randint(20, 40)
     location = "Brazil"
-    sql = "INSERT INTO temperatura (temperatura, location, memoria) VALUES (?, ?, ?)"
+    sql = "INSERT INTO temperatura (temperatura, regiao, memoria) VALUES (?, ?, ?)"
     val = (temperature, location, max_mem)
     start_insert = time.time()
     cursor.execute(sql, val)
